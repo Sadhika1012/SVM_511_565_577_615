9b3a3c194a26e6ef35fc00f7e4cee3ed25e64f8e
Shreeya Rao <106077939+Shreeyasatish@users.noreply.github.com>
2024-09-03 22:16:41 +0530

    Add files via upload

--- frontend/src/pages/Admin/Admin.js
+++ frontend/src/pages/Admin/Admin.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Admin.css'; // Import the CSS file
 
+const Admin = () => {
+  const navigate = useNavigate();
+
+  const handleViewGraph = () => {
+    navigate('/graph');
+  };
+
+  const handleFlaggedClones = () => {
+    navigate('/flagged-clones');
+  };
+
+  return (
+    <div className="container">
+      <button className="button" onClick={handleViewGraph}>View Graph</button>
+      <button className="button" onClick={handleFlaggedClones}>Flagged Clones</button>
+    </div>
+  );
+};
+
+export default Admin;
