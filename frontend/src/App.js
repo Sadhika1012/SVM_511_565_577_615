28d7ce3ccacf9c46d32b317c522b1fe886fe54e9
Shreeya Rao <106077939+Shreeyasatish@users.noreply.github.com>
2024-09-03 22:17:26 +0530

    changed App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Graph from './pages/Graph/Graph';
+import Admin from './pages/Admin/Admin';
 import Login from './pages/Login/Login';
 import Profile from './pages/Profile/Profile';
+import Graph from './pages/Graph/Graph';
 function App() {  
   return (
     <Router>
       <Routes>
-      <Route path="/graph" element={<Graph />} />
+      <Route path="/admin" element={<Admin />} />
       <Route path="/login" element={<Login />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/graph" element={<Graph />} />
       </Routes>      
     </Router>
   );
