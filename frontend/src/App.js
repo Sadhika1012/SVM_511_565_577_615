43ce97eccbc2d5b0127b72832c9e945c9bcf5d29
Shreeya Rao <106077939+Shreeyasatish@users.noreply.github.com>
2024-10-15 19:30:42 +0530

    Added routes to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Admin from './pages/Admin/Admin';
 import Login from './pages/Login/Login';
 import Profile from './pages/Profile/Profile';
 import Graph from './pages/Graph/Graph';
+import FlaggedClones from './pages/Admin/FlaggedClones';
 function App() {  
   return (
     <Router>
@@ -11,6 +12,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/graph" element={<Graph />} />
+      <Route path="/flagged-clones" element={<FlaggedClones />}/>
       </Routes>      
     </Router>
   );
